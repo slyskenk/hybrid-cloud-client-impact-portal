140a122c2da4f6902790fa1abe4a5a5701102b8c
Amberlight Adventures <amberlightadventures@gmail.com>
2026-05-20 23:41:00 +0200

    Enable basic auth for API testing

--- src/main/java/com/ibmjob/hybridportal/config/SecurityConfig.java
+++ src/main/java/com/ibmjob/hybridportal/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ibmjob.hybridportal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,6 +36,7 @@ public class SecurityConfig {
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
                 )
+                .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf
                         .ignoringRequestMatchers(
                                 new AntPathRequestMatcher("/h2-console/**"),
